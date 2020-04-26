fb89a57d0811b04fe0b73fd8a2c2c0417f07eeca
Menaka Sirisena <menaka.s@eyepax.com>
2020-04-26 11:43:29 +0530

    passed an event from nested component to the contained component

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -61,4 +61,8 @@ export class ProductListComponent implements OnInit {
     ngOnInit(): void {
       console.log('in OnInit');
     }
+
+    onRatingClicked(message: string): void {
+      this.pageTitle = 'Product List: ' + message;
+     }
 }
--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
     selector: 'pm-star',
     templateUrl: './star.component.html',
     styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnChanges {
+export class StarComponent implements OnChanges, OnInit {
     @Input() rating: number;
     starWidth: number;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
     ngOnChanges(): void {
         this.starWidth = this.rating * 75 / 5;
+    }
+
+    ngOnInit(): void {
+        this.starWidth = this.rating * 75 / 5;
+    }
+
+    onClick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+        console.log(`The rating ${this.rating} was clicked!`);
     }
 }
