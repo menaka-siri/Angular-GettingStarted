131acf8ffd4ea8cc2f862e78f9227dd5905c2085
eyepax_MenakaS <menaka.sirisena@eyepax.com>
2020-05-10 09:44:46 +0530

    added router link to product name in product-list page, added back button in product-detail page, formatted styles in product-list page

--- APM-Start/src/app/products/product-detail/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from '../products';
 
 
@@ -12,9 +14,25 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    let id= +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 18, 2019",
+      "description": "Leaf Rake with 48-inch wooden handle",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    };
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
